4b90872f32e7be8d547f3df3d4769b6780a14b98
shgtkshruch <shgtk.shruch@gmail.com>
2014-10-28 17:29:31 +0900

    Get issue contents.

--- index.js
+++ index.js
@@ -14,17 +14,59 @@ function getHtml(cb) {
 
     res.on('end', function (err) {
       if (err) {
-        cd(err);
+        cb(err);
       }
       cb(null, data);
     });
   });
 }
 
+function getIssue(html, cb) {
+  var $ = cheerio.load(html);
+  var issue = {};
+  var articles = [];
+
+  issue.title = $('.newsletter-head > h1').text();
+  issue.date = $('.newsletter-head > time').text();
+
+  $('.newsletter-article').each(function (i, elem) {
+
+    if ($(this).prev('.section-title').text().search(/Sponsor/) !== -1) {
+      return;
+    }
+
+    var article = {};
+
+    article.title = $(this).find('.article-title > a').text();
+
+    article.body = $(this).find('p').text();
+
+    article.url =  $(this).find('.article-title').html().split('"')[1].split('?')[0];
+
+    var img = $(this).find('img').parent().html();
+    article.image = img ? img.split('src=')[1].split('"')[1] : null;
+
+    article.cat = $(this).parent().children('h2').first().text() || 'Article & Toutorials';
+
+    article.source = article.cat === 'Tools' ? null : $(this).find('.article-title + a').text();
+
+    articles[i] = article;
+  });
+  issue.articles = articles;
+
+  cb(null, issue);
+}
+
 async.waterfall([
   function (cb) {
     getHtml(cb);
+  },
+  function (html, cb) {
+    getIssue(html, cb);
   }
 ], function (err, result) {
+  if (err) {
+    throw err;
+  }
   console.log(result);
 });
